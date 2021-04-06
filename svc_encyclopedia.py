956877261b797c075b43daa7c98820bf95c841ac
David Shapiro <daveshap37@gmail.com>
2021-04-06 14:36:21 -0400

    fixed recall, ready to test recall and encyclopedia

--- svc_encyclopedia.py
+++ svc_encyclopedia.py
@@ -26,16 +26,7 @@ def query_nexus():
     for i in stream:
         if i['time'] > last_msg['time']:
             last_msg = i
-
     return stream
-
-
-def save_nexus(messages, connection, cursor):
-    # order: msg, key, sid, irt, ctx, mid, time
-    values = [(i['msg'], i['key'], i['sid'], i['irt'], i['ctx'], i['mid'], i['time']) for i in messages if 'recall' not in i['key']]  # exclude recalled messages
-    for value in values:
-        result = cursor.execute('INSERT OR IGNORE INTO recall VALUES (?,?,?,?,?,?,?)', value)  # skip over duplicate values
-    connection.commit()
 
 
 def find_keywords(messages):
@@ -44,34 +35,31 @@ def find_keywords(messages):
     for message in messages:
         r.extract_keywords_from_text(message['msg'])
         scores = r.get_ranked_phrases_with_scores()
-        terms = [i[1] for i in scores if i[0] > 2.0]  # generally seems like nothing below 2.0 is interesting
+        terms = [(i[1], message['ctx']) for i in scores if i[0] > 2.0]  # generally seems like nothing below 2.0 is interesting
         results += terms
     return results
 
 
-def fetch_recall(terms, cursor):
+def fetch_encyclopedia(terms, cursor):
     results = list()
     for term in terms:
         # TODO split term up based on whitespace, remove punctuation, add multiple LIKE clauses to WHERE condition
-        query = """SELECT * FROM recall WHERE msg LIKE '%{0}%'""".format(term)
+        query = """SELECT article FROM wiki WHERE article LIKE '%{0}%'""".format(term[0])
         cursor.execute(query)
         fetch = cursor.fetchall()
-        results += fetch
-    final = [{'msg': i[0], 'key': i[1], 'sid': i[2], 'irt': i[3], 'ctx': i[4], 'mid': i[5], 'time': i[6]} for i in results]
-    return final
+        results += [(i, term[1]) for i in fetch]  # i is article, term[1] is CTX
+    return results
 
 
-def post_messages(messages):
-    for message in messages:
+def post_messages(articles):
+    for article in articles:
         try:
             payload = dict()
-            payload['msg'] = message['msg']
-            payload['irt'] = message['irt']
-            payload['ctx'] = message['ctx']
-            payload['mid'] = message['mid']  # nexus can decide whether or not to keep
-            payload['time'] = message['time']  # nexus can decide whether or not to keep
-            payload['key'] = 'recall.' + message['key']
-            payload['sid'] = 'recall.' + message['sid']
+            payload['msg'] = article[0]
+            payload['irt'] = article[1]
+            payload['ctx'] = article[1]
+            payload['key'] = 'encyclopedia.article'
+            payload['sid'] = 'encyclopedia.wiki.simple'
             result = nexus_post(payload)
             #print(result)
         except Exception as oops:
@@ -80,13 +68,11 @@ def post_messages(messages):
 
 if __name__ == '__main__':
     print('Starting Recall Service')
-    dbcon = sqlite3.connect('raven_recall.sqlite')
+    dbcon = sqlite3.connect('simple_wiki.sqlite')
     dbcur = dbcon.cursor()
-    start_db(dbcon, dbcur)
     while True:
         latest = query_nexus()
-        save_nexus(latest, dbcon, dbcur)
         keywords = find_keywords(latest)
-        recall_messages = fetch_recall(keywords, dbcur)
-        post_messages(recall_messages)
+        encyclopedia_articles = fetch_encyclopedia(keywords, dbcur)
+        post_messages(encyclopedia_articles)
         sleep(default_sleep)
